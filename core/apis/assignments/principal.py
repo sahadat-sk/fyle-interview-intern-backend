b254e22a09f16ada0ae4dc58e9d30584cc846d5e
sahadat-sk <mss.21U10442@btech.nitdgp.ac.in>
2024-05-13 19:35:34 +0530

    add: principal/assignments route

---
+++ core/apis/assignments/principal.py
@@ -0,0 +1,19 @@
+from flask import Blueprint
+from core import db
+from core.apis import decorators
+from core.apis.responses import APIResponse
+from core.models.assignments import Assignment
+
+from .schema import AssignmentSchema, AssignmentGradeSchema
+principal_assignments_resources = Blueprint('principal_assignments_resources', __name__)
+
+
+@principal_assignments_resources.route('/assignments', methods=['GET'], strict_slashes=False)
+@decorators.authenticate_principal
+def list_assignments(p):
+    """Returns list of assignments"""
+    submitted_and_graded_assignments = Assignment.get_all_submitted_and_graded_assignments()
+    submitted_and_graded_assignments_dump = AssignmentSchema().dump(submitted_and_graded_assignments, many=True)
+    return APIResponse.respond(data=submitted_and_graded_assignments_dump)
+
+
--- core/models/assignments.py
+++ core/models/assignments.py
@@ -91,3 +91,7 @@ class Assignment(db.Model):
     @classmethod
     def get_assignments_by_teacher(cls):
         return cls.query.all()
+    
+    @classmethod
+    def get_all_submitted_and_graded_assignments(cls):
+        return cls.filter((cls.state == AssignmentStateEnum.SUBMITTED) | (cls.state == AssignmentStateEnum.GRADED)).all()
