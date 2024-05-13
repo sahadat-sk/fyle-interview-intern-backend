1b4da7fcb8abc9aab84795c8d3d782baea54a674
sahadat-sk <mss.21U10442@btech.nitdgp.ac.in>
2024-05-13 20:27:22 +0530

    fix: assignments with empty content cannot be submitted

--- core/models/assignments.py
+++ core/models/assignments.py
@@ -50,10 +50,12 @@ class Assignment(db.Model):
             assertions.assert_found(assignment, 'No assignment with this id was found')
             assertions.assert_valid(assignment.state == AssignmentStateEnum.DRAFT,
                                     'only assignment in draft state can be edited')
+            assertions.assert_valid(assignment.content is not None,'content cannot be empty')
 
             assignment.content = assignment_new.content
         else:
             assignment = assignment_new
+            assertions.assert_valid(assignment.content is not None,'content cannot be empty')
             db.session.add(assignment_new)
 
         db.session.flush()
