bad2d601d8dcbf12f778ba1258b49cd21e406946
sahadat-sk <mss.21U10442@btech.nitdgp.ac.in>
2024-05-13 21:00:55 +0530

    fix: only principal or the assigned teacher can grade the student

--- core/models/assignments.py
+++ core/models/assignments.py
@@ -82,6 +82,7 @@ class Assignment(db.Model):
         assertions.assert_found(assignment, 'No assignment with this id was found')
         assertions.assert_valid(grade is not None, 'assignment with empty grade cannot be graded')
         assertions.assert_valid(assignment.state != AssignmentStateEnum.DRAFT,'draft assignments cannot be graded')
+        assertions.assert_valid((auth_principal.principal_id is not None) | (assignment.teacher_id == auth_principal.teacher_id),'This assignment was submitted to another teacher')
 
         assignment.grade = grade
         assignment.state = AssignmentStateEnum.GRADED
