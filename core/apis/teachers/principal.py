bc4d8833128fb1852be9d7b88abf532ca618ee22
sahadat-sk <mss.21U10442@btech.nitdgp.ac.in>
2024-05-14 14:22:16 +0530

    add: missing tests

--- core/apis/teachers/principal.py
+++ core/apis/teachers/principal.py
@@ -1,8 +1,6 @@
 from flask import Blueprint
-from core import db
 from core.apis import decorators
 from core.apis.responses import APIResponse
-from core.models.assignments import Assignment
 from core.models.teachers import Teacher
 
 from .schema import TeacherSchema
